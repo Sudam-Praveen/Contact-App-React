b9781ca6c74ffb1e3a57f9a18e7e40563c0e2b8c
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-03-21 21:28:40 +0530

    setting-up

---
+++ contact-app/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import ContactList from './ContactList'
+import AddContact from './AddContact'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>Hello wrold</div>
+  )
+}
+
+export default App
